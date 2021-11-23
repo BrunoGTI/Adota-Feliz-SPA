585698d53710c243b56c35de423818b6da839ab2
BrunoGTI <brunoh14pb@gmail.com>
2021-11-23 10:25:22 -0300

    update name project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Adota-Mais-SPA'),
+      dir: require('path').join(__dirname, './coverage/Adota-Feliz-SPA'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Adota-Mais-SPA'`, () => {
+  it(`should have as title 'Adota-Feliz-SPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Adota-Mais-SPA');
+    expect(app.title).toEqual('Adota-Feliz-SPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Adota-Mais-SPA app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Adota-Feliz-SPA app is running!');
   });
 });
